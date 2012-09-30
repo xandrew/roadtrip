5457bc9e57b097d1e939c95680798f869c7bfc1c
xandrew <xxandreww@gmail.com>
2012-09-30 18:10:28 +0200

    Primitive path editor.

--- roadtrip/static/pather.js
+++ roadtrip/static/pather.js
@@ -11,16 +11,103 @@ function Pather() {
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
 
-  $('#drive').click(function() {
-		      var dir_request = {
-			origin: $("#from").val(),
-			destination: $("#to").val(),
-			provideRouteAlternatives: false,
-			travelMode: google.maps.TravelMode.WALKING,
-			unitSystem: google.maps.UnitSystem.METRIC
-		      };
-		      directions_service_.route(dir_request, receiveDirections);
-		    });
+  var div_ = $('#route_controlls');
+  var title_ = $('#rc_title');
+
+  var path_steps_ = [];
+
+  function PathStep(previous_step, next_step) {
+    var prev_ = previous_step;
+    var next_ = next_step;
+
+    var step_div_ = $('<div></div>');
+    var input_ = $('<input type=text></input>');
+    step_div_.append(input_);
+
+    if (prev_ !== undefined) {
+      step_div_.insertAfter(prev_.Div());
+    } else {
+      step_div_.insertAfter(title_);
+    }
+
+    var api_ = {
+      'Div': function() { return step_div_; },
+      'Next': function() { return next_; },
+      'Prev': function() { return prev_; },
+      'SetNext': function(next) { next_ = next; },
+      'SetPrev': function(prev) { prev_ = prev; },
+      'Value': function() { return input_.val(); }
+    };
+
+    var pin_button_ = $('<button>!</button>');
+    step_div_.append(pin_button_);
+    pin_button_.click(
+      function() {
+	google.maps.event.addListenerOnce(map_, 'click', function(e) {
+					    input_.val(e.latLng.toString());
+					  });
+      });
+
+
+    var add_button_ = $('<button>+</button>');
+    step_div_.append(add_button_);
+    add_button_.click(
+      function() {
+	next_ = PathStep(api_, next_);
+      });      
+
+    var remove_button_ = $('<button>-</button>');
+    step_div_.append(remove_button_);
+    remove_button_.click(
+      function() {
+	if ((prev_ !== undefined) && (next_ !== undefined)) {
+	  prev_.SetNext(next_);
+	  next_.SetPrev(prev_);
+	  step_div_.remove();
+	}
+      });
+
+  
+    return api_;
+  }
+
+  var first_step_ = PathStep();
+
+  $('#drive').click(
+    function() {
+      var origin;
+      var destination;
+      var waypoints = [];
+      var act_step = first_step_;
+      while (act_step !== undefined) {
+	var used = false;
+	if (origin === undefined) {
+	  origin = act_step.Value();
+	  used = true;
+	}
+	var next_step = act_step.Next();
+	if (next_step === undefined) {
+	  destination = act_step.Value();
+	  used = true;
+	}
+	if (!used) {
+	  waypoints.push({
+			   location: act_step.Value(),
+			   stopover: false
+			 });
+	}
+	act_step = next_step;
+      }
+      var dir_request = {
+	origin: origin,
+	destination: destination,
+	waypoints: waypoints,
+	provideRouteAlternatives: false,
+	travelMode: google.maps.TravelMode.WALKING,
+	unitSystem: google.maps.UnitSystem.METRIC
+      };
+      directions_service_.route(dir_request, receiveDirections);
+    });
   function receiveDirections(result, status) {
     if (status != google.maps.DirectionsStatus.OK) {
       alert("Error");
@@ -28,13 +115,15 @@ function Pather() {
       var path = result.routes[0].overview_path;
       console.log(JSON.stringify(path));
       map_.panTo(path[0]);
+      if (path_line_ !== undefined) {
+	path_line_.setMap(null);
+      }
       path_line_ = new google.maps.Polyline(
 	{
 	  path: result.routes[0].overview_path,
 	  strokeColor: "#FF0000",
 	  strokeOpacity: 1.0,
-	  strokeWeight: 2,
-	  editable: true});
+	  strokeWeight: 2});
       path_line_.setMap(map_);
     }
   }
